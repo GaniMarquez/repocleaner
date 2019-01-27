79399427e8aae41fc90cab1dac3c439e012f2176
Isagani Marquez <isaganimarquez@192.168.254.111>
2019-01-27 13:59:10 +0800

    Added owner to arguments

--- repocleaner.py
+++ repocleaner.py
@@ -1,35 +1,33 @@
-import os
 import argparse
 import requests
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--cleanup', '-c',
-                        action='store_true',
-                        help='')
     parser.add_argument('--file', '-f',
-                        default='repos',
+                        required=True,
+                        help='')
+    parser.add_argument('--owner', '-o',
+                        required=True,
                         help='')
     parser.add_argument('--token', '-t',
-                        default=None,
+                        required=True,
                         help='')
     args = parser.parse_args()
     return args
 
-def delete_repositories(token, file):
+def delete_repositories(owner, token, file):
     headers = {'Authorization': 'token {token}'.format(token=token)}
     with open(file) as file:
         repos = map(lambda x: x.strip(), file.readlines())
         for repo in repos:
-            url = 'https://api.github.com/repos/{repo}'.format(repo=repo)
+            url = 'https://api.github.com/repos/{owner}/{repo}'.format(owner=owner, repo=repo)
             requests.delete(url, headers=headers)
-            print 'Succesfully deleted {repo}'.format(repo=repo)
-    os.system('rm {file}'.format(file=file))
+            print('Succesfully deleted {repo}'.format(repo=repo))
 
 def main():
     args = parse_arguments()
-    delete_repositories(args.token, args.file)
+    delete_repositories(args.owner, args.token, args.file)
 
 if __name__ == '__main__':
     main()
